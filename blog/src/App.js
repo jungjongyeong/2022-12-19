f2ca80a93dd78ed77722946118d1887489938bd8
jjy jeong <asde621@naver.com>
2022-12-20 01:20:06 +0900

    commit2

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,11 +1,17 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','여자 코트 추천','남자 목걸이 추천','남자 목도리 추천']);
+  let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집';
-  function 함수(){
-    return 100;
+
+  function 제목바꾸기(){
+    var newArray = [...글제목];
+    newArray[0] = '여자코트 추천';
+    글제목변경( newArray );
   }
 
   return (
@@ -13,8 +19,18 @@ function App() {
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <img src={logo}></img>
-      <h4>{ 함수() }</h4>
+      <button onClick={ 제목바꾸기 }>버튼</button>
+      <div className="list">
+        <h3> { 글제목[0] }<span onClick={ ()=>{ 따봉변경(따봉 + 1)}}>👍</span>{따봉}</h3>
+        <p>2월 17일 발행</p>
+        <h3> { 글제목[1] }</h3>
+        <p>2월 16일 발행</p>
+        <h3> { 글제목[2] }</h3>
+        <p>2월 15일 발행</p>
+        <h3> { 글제목[3] }</h3>
+        <p>2월 12일 발행</p>
+
+      </div>
     </div>
   );
 }
